ed3042ba7ebda80e174ef1f1edd0721943b7ea0e
='Marco <='maanmuca@hotmail.com>
2018-12-12 08:17:21 +1100

    Profile Page 1

--- adboybots/src/pages/adboyBotsProfile.vue
+++ adboybots/src/pages/adboyBotsProfile.vue
@@ -73,19 +73,88 @@
                             <v-expansion-panel-content>
                             <div slot="header" color="black">Opening Hours</div>
                             <v-card>
-                                <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</v-card-text>
+                                <v-layout class="pa-2">
+                                <v-flex xs4 >
+                                    <v-select
+                                    v-model="select"
+                                    :items="openingDays"
+                                    :rules="[v => !!v || 'Item is required']"
+                                    label="Opening Days"
+                                    required
+                                    ></v-select>
+                                </v-flex>
+                                    
+                                <v-flex xs4 >
+                                    <v-dialog
+                                        ref="dialog"
+                                        v-model="modal2"
+                                        :return-value.sync="time"
+                                        persistent
+                                        lazy
+                                        full-width
+                                        width="290px"
+                                    >
+                                        <v-text-field
+                                        slot="activator"
+                                        v-model="time"
+                                        label="Opening"
+                                        prepend-icon="access_time"
+                                        readonly
+                                        ></v-text-field>
+                                        <v-time-picker
+                                        v-if="modal2"
+                                        v-model="time"
+                                        full-width
+                                        >
+                                        <v-spacer></v-spacer>
+                                        <v-btn flat color="primary" @click="modal2 = false">Cancel</v-btn>
+                                        <v-btn flat color="primary" @click="$refs.dialog.save(time)">OK</v-btn>
+                                        </v-time-picker>
+                                    </v-dialog>
+                                </v-flex>
+                                <v-flex xs4>
+                                    <v-dialog
+                                        ref="dialog"
+                                        v-model="modal2"
+                                        :return-value.sync="time"
+                                        persistent
+                                        lazy
+                                        full-width
+                                        width="290px"
+                                    >
+                                        <v-text-field
+                                        slot="activator"
+                                        v-model="time"
+                                        label="Closing"
+                                        prepend-icon="access_time"
+                                        readonly
+                                        ></v-text-field>
+                                        <v-time-picker
+                                        v-if="modal2"
+                                        v-model="time"
+                                        full-width
+                                        >
+                                        <v-spacer></v-spacer>
+                                        <v-btn flat color="primary" @click="modal2 = false">Cancel</v-btn>
+                                        <v-btn flat color="primary" @click="$refs.dialog.save(time)">OK</v-btn>
+                                        </v-time-picker>
+                                    </v-dialog>
+                                </v-flex>
+                                </v-layout>
                             </v-card>
                             </v-expansion-panel-content>
                         </v-expansion-panel>
                     </v-flex>
+                    <v-flex  xs12 class="ma-2">
                      <v-btn
-                    :disabled="!valid"
-                    @click="submit"
-                    class="info"
-                    >
-                    submit
-                    </v-btn>
-                    <v-btn @click="clear" class="info">clear</v-btn>
+                        :disabled="!valid"
+                        @click="submit"
+                        class="info"
+                     >
+                        submit
+                     </v-btn>
+                     <v-btn @click="clear" class="info">Define attributes</v-btn>
+                    </v-flex>
                 </v-card>
             </v-flex>
             </v-layout>
@@ -118,7 +187,19 @@ import adboyBotsUsersHeader from '@/components/adboyBotsUsersHeader'
         'Education',
         'Hobbie'
       ],
+      openingDays:[
+          'Monday',
+          'Tuesday',
+          'Wednesday',
+          'Thursday',
+          'Friday',
+          'Saturday',
+          'Sunday'
+      ],
       checkbox: false,
+      time: null,
+      menu2: false,
+      modal2: false,
 
         methods: {
         submit () {
